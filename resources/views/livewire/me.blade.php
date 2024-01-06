a8d929a3fb9b9621a4c361aa88b9663862433940
Bùi Hữu Lộc <feature451@gmail.com>
2024-01-07 00:57:25 +0700

    chore

--- resources/views/livewire/me.blade.php
+++ resources/views/livewire/me.blade.php
@@ -1,22 +1,22 @@
+@php use \Illuminate\Support\Facades\Auth; @endphp
 <div>
     <div class="membership-account membership-header section-profile is-cover">
         <div class="profile-wrap is-cover" style="background-image: url(https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib&#x3D;rb-1.2.1&amp;q&#x3D;75&amp;fm&#x3D;jpg&amp;crop&#x3D;entropy&amp;cs&#x3D;tinysrgb&amp;w&#x3D;2000&amp;fit&#x3D;max&amp;ixid&#x3D;eyJhcHBfaWQiOjExNzczfQ)">
             <h1 class="membership-name">
                 Your account
             </h1>
-            <h2>Currently, you are not a paying subscriber.</h2>
             <div class="membership-details">
                 <div class="membership-details-wrap flex">
                     <div class="membership-details-content free">
                         <div class="membership-detail free">
                             <label>Email address</label>
-                            <span>feature451@gmail.com</span>
+                            <span>{{ Auth::user()->email }}</span>
                         </div>
                     </div>
                     <div class="membership-details-content free">
                         <div class="membership-detail free">
                             <label>Your plan</label>
-                            <span>{{ \Illuminate\Support\Facades\Auth::user()->premiumDescription }}</span>
+                            <span>{{ Auth::user()->premiumDescription }}</span>
                         </div>
                     </div>
 
