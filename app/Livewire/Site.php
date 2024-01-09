4822a0cb533b9b73ad62045cdf022be2bdcb2d16
Bùi Hữu Lộc <feature451@gmail.com>
2024-01-10 00:50:26 +0700

    fix: sort confs desc

--- app/Livewire/Site.php
+++ app/Livewire/Site.php
@@ -17,11 +17,9 @@ class Site extends Component
 
     public function mount(): void
     {
-        $this->slider_confessions = Confession::query()->whereNotNull('banner')->take(3)->get();
-        $p_confessions = Confession::query()->whereNotIn('id', $this->slider_confessions->pluck('id')->toArray())->paginate(9);
-        $this->confessions = $p_confessions->items();
-        $this->page_count = $p_confessions->lastPage();
-        $this->cur_page = $p_confessions->currentPage();
+        $this->slider_confessions = Confession::query()->whereNotNull('banner')->orderByDesc('created_at')->take(3)->get();
+        $confessions = Confession::query()->whereNotIn('id', $this->slider_confessions->pluck('id')->toArray())->orderByDesc('created_at')->paginate(9);
+        [$this->confessions, $this->page_count, $this->cur_page] = [$confessions->items(), $confessions->lastPage(), $confessions->currentPage()];
     }
 
 }
