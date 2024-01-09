f1937bbaf271c376dbd13ec9f6c7b41eea06fdff
Bùi Hữu Lộc <feature451@gmail.com>
2024-01-09 23:12:21 +0700

    fix: view collection

--- app/Livewire/Collection.php
+++ app/Livewire/Collection.php
@@ -13,6 +13,7 @@ class Collection extends Component
     public $confessions;
     public int $page_count;
     public int $cur_page;
+    public int $total;
 
     public function mount(Request $request): void
     {
@@ -20,14 +21,14 @@ class Collection extends Component
             $this->redirectRoute('index');
             return;
         }
-        $this->page = match ($page) { 'history' => 'History', 'watch-later' => 'Watch later', 'liked' => 'Liked' };
+        $this->page = match ($page) { 'history' => 'Lịch sử', 'watch-later' => 'Xem sau', 'liked' => 'Đã thích' };
         $confessions = match($page) {
             'history' => Auth::user()->historyConfessions()->paginate(9),
             'watch-later' => Auth::user()->watchLaterConfessions()->paginate(9),
             'liked' => Auth::user()->likedConfessions()->paginate(9),
         };
 
-        [$this->confessions, $this->page_count, $this->cur_page] = [$confessions->items(), $confessions->lastPage(), $confessions->currentPage()];
+        [$this->confessions, $this->page_count, $this->cur_page, $this->total] = [$confessions->items(), $confessions->lastPage(), $confessions->currentPage(), $confessions->total()];
     }
 
 }
